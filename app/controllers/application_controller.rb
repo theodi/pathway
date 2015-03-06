1a31762f51d76db96ad4b6e5fe9a7cc7f26b3f20
digitalWestie <giannichan@gmail.com>
2015-03-06 15:15:21 +0000

    permits associated_organisation as a parameter for sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :configure_permitted_parameters, if: :devise_controller?
   
   def index
   end  
   
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :associated_organisation
+  end
+
 end
