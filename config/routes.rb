514cb01d9371ee21dccff9cb629d8201f548a37c
digitalWestie <giannichan@gmail.com>
2015-03-06 17:26:52 +0000

    adds route to organisation action

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :user, :controller => "user"
   
+  get 'organisations' => 'organisations#index', as: 'organisations'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
