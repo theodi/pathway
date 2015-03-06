5f0bfa4fabb212e29788f61ce3f3d42967fb2c3d
digitalWestie <giannichan@gmail.com>
2015-03-06 15:38:49 +0000

    adds further test coverage for registrations controller

--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -14,15 +14,30 @@ describe RegistrationsController do
   end
   
   describe "POST #create" do
-    it "should allow a new user to be created with an organisation" do
+    attrs = FactoryGirl.attributes_for(:user, associated_organisation: "DoSAC")
+    it "should allow a new user to be created with a new organisation" do
       expect{  
-        post :create, user: FactoryGirl.attributes_for(:user), organisation: "British Waterways"
+        post :create, user: attrs
+      }.to change(User,:count).by(1)
+    end
+
+    it "should create a new organisation for the user" do
+      attrs = FactoryGirl.attributes_for(:user, associated_organisation: "DoSAC")
+      expect{  
+        post :create, user: attrs 
+      }.to change(Organisation,:count).by(1)
+    end
+
+    it "should allow a new user to be created with an existing organisation" do
+      FactoryGirl.create(:organisation) 
+      expect{ 
+        post :create, user: FactoryGirl.attributes_for(:user), associated_organisation: "Department for Environment, Food and Rural Affairs"
       }.to change(User,:count).by(1)
     end
 
     it "should allow a new user to be created without an organisation" do
       expect{
-        post :create, user: FactoryGirl.attributes_for(:user), organisation: ""
+        post :create, user: FactoryGirl.attributes_for(:user), associated_organisation: ""
       }.to change(User,:count).by(1)
     end
   end
