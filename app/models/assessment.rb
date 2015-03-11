7754bf1bb5904694f719f358f471177304536a8f
digitalWestie <giannichan@gmail.com>
2015-03-11 16:16:56 +0000

    adds status to assessment and displays in index

--- app/models/assessment.rb
+++ app/models/assessment.rb
@@ -1,4 +1,14 @@
 class Assessment < ActiveRecord::Base
   belongs_to :user
   has_many :assessment_answer, dependent: :destroy
+
+  def status
+    if start_date.blank?
+      "Not started"
+    elsif completion_date.blank?
+      "Incomplete"
+    else
+      "Completed"
+    end 
+  end
 end
