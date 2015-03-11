477936d6a0b30969665fe0f171a92f378c8aec8e
digitalWestie <giannichan@gmail.com>
2015-03-11 15:28:36 +0000

    add assessments path helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
-  get 'assessments/index'
-
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :user, :controller => "user"
   
   get 'organisations' => 'organisations#index', as: 'organisations'
+  get 'assessments' => 'assessments#index', as: 'assessments'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
