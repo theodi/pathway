f5854a2885cd96ce81958a6ca70c8b0a129f535c
digitalWestie <giannichan@gmail.com>
2015-03-12 12:11:53 +0000

    adds method to pick out current asssessment and adds pretty date helper

--- app/controllers/assessments_controller.rb
+++ app/controllers/assessments_controller.rb
@@ -1,6 +1,7 @@
 class AssessmentsController < ApplicationController
   def index
-    @assessments = current_user.assessments.order(:completion_date)
+    @current_assessment = current_user.current_assessment
+    @assessments = current_user.assessments.completed.order(:completion_date)
   end
 
   def destroy
--- app/models/assessment.rb
+++ app/models/assessment.rb
@@ -2,6 +2,10 @@ class Assessment < ActiveRecord::Base
   belongs_to :user
   has_many :assessment_answer, dependent: :destroy
 
+  def self.completed
+    where.not(completion_date: nil)
+  end
+
   def status
     if start_date.blank?
       "Not started"
