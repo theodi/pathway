9ba352838b34584acd85ac5fd39fae2fe05fd284
Leigh Dodds <leigh@ldodds.com>
2015-03-13 18:06:14 +0000

    Reworked DatabaseCleaner integration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -37,7 +37,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # If true, the base class of anonymous controllers will be inferred
   # automatically. This will be the default behavior in future versions of
@@ -66,11 +66,15 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   
   config.include Devise::TestHelpers, :type => :controller
-  
-  
+    
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation,  {except: %w[public.schema_migrations]})
+    #DatabaseCleaner.strategy = :truncation, {except: %w[public.schema_migrations]}
   end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+    end
 
   config.before(:each) do
     DatabaseCleaner.start
