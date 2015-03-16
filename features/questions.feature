20caeecb3f5ffa6483af03da951cd2acb4c309c1
digitalWestie <giannichan@gmail.com>
2015-03-16 14:09:27 +0000

    adds cucumber coverage for redirecting to next question

---
+++ features/questions.feature
@@ -0,0 +1,9 @@
+Feature: Answering questions
+
+  Scenario: Answering a question positively
+    Given I am logged in as a user
+    Given I have started an assessment
+    When I go to the first question
+    And I choose "Yes"
+    And I press "Save"
+    Then I should see "Have you released an open data schedule?"
---
+++ features/step_definitions/question_steps.rb
@@ -0,0 +1,15 @@
+Given(/^I have started an assessment$/) do
+  @assessment = FactoryGirl.create(:unfinished_assessment, user_id: @current_user.id)
+  dimension = FactoryGirl.create(:dimension)
+  @activity = dimension.activities.create(FactoryGirl.attributes_for(:activity))
+  @activity.questions.create(FactoryGirl.attributes_for(:question))
+  @activity.questions.create(FactoryGirl.attributes_for(:question, code: "q2", text: "Have you released an open data schedule?"))
+  @activity.questions.create(FactoryGirl.attributes_for(:question, code: "q3", text: "Do you have an open data officer?"))
+  
+  FactoryGirl.create(:answer)
+  FactoryGirl.create(:negative_answer)
+end
+
+When(/^I go to the first question$/) do
+  visit assessment_question_path(@assessment, @activity.questions.first)
+end
