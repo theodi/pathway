dfc2a95db2298963698afd9ff4237a92c2eecf11
digitalWestie <giannichan@gmail.com>
2015-03-18 18:20:48 +0000

    allows for nested links in assessment answers

--- app/models/assessment_answer.rb
+++ app/models/assessment_answer.rb
@@ -3,4 +3,6 @@ class AssessmentAnswer < ActiveRecord::Base
   belongs_to :question
   belongs_to :answer
   has_many :links, dependent: :destroy
+
+  accepts_nested_attributes_for :links, allow_destroy: true
 end
