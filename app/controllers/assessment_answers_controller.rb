85a13f29e686bc4acfe4e7e65cea3ce509a0b6d7
digitalWestie <giannichan@gmail.com>
2015-03-18 18:19:14 +0000

    refactor and adds edit and update actions for assessment answers

--- app/controllers/assessment_answers_controller.rb
+++ app/controllers/assessment_answers_controller.rb
@@ -1,22 +1,18 @@
 class AssessmentAnswersController < ApplicationController
 
+  before_filter :load_and_authorize_assessment
+
   def new
-    @assessment = current_user.assessments.find(params[:assessment_id])
     @question = Question.find(params[:question_id])
     @activity = @question.activity
     @dimension = @activity.dimension
-    authorize! :update, @assessment
-
     @assessment_answer = @assessment.assessment_answers.build(question: @question)
   end
 
   def create
-    @assessment = current_user.assessments.find(params[:assessment_id])
     @question = Question.find(params[:question_id])
     @activity = @question.activity
     @dimension = @activity.dimension
-    authorize! :update, @assessment
-
     @assessment_answer = @assessment.assessment_answers.build(assessment_answer_params)
 
     if @assessment_answer.save
@@ -29,16 +25,36 @@ class AssessmentAnswersController < ApplicationController
   end
 
   def edit
+    @assessment_answer = @assessment.assessment_answers.find(params[:id])
+    @question = @assessment_answer.question
+    @activity = @question.activity
+    @dimension = @activity.dimension
   end
 
   def update
+    @assessment_answer = @assessment.assessment_answers.find(params[:id])
+    @question = @assessment_answer.question
+    @activity = @question.activity
+    @dimension = @activity.dimension
+    if @assessment_answer.update_attributes(assessment_answer_params)
+      next_question = @activity.next_question_for(@assessment)
+      redirection = next_question.blank? ? assessment_path(@assessment) : assessment_question_path(@assessment, next_question)
+      redirect_to redirection
+    else
+      render 'edit'
+    end
   end
 
   private
 
   def assessment_answer_params
     params[:assessment_answer].merge!( { question_id: params[:question_id] } ) if params[:question_id]
-    params.require(:assessment_answer).permit(:question_id, :answer_id, :notes)
+    params.require(:assessment_answer).permit(:question_id, :answer_id, :notes, links_attributes: [:text, :link, :_destroy, :id])
+  end
+
+  def load_and_authorize_assessment
+    @assessment = current_user.assessments.find(params[:assessment_id])
+    authorize! :update, @assessment
   end
 
 end
