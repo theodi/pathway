008edb2c803a50d3f37e66848181b91f051d6f80
digitalWestie <giannichan@gmail.com>
2015-03-20 16:18:56 +0000

    adds name to user factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :user do
+    name "Peter Manion"
     email "user@example.org"
     password 'somepassword'
     encrypted_password Devise.bcrypt(User, 'somepassword')
@@ -7,6 +8,7 @@ FactoryGirl.define do
 
   # This will use the User class (Admin would have been guessed)
   factory :admin, class: User do
+    name "Peter Manion"
     email "admin@example.org"
     password 'otherpassword'
     encrypted_password Devise.bcrypt(User, 'otherpassword')
