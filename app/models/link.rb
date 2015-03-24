38baf1c07e8a2f28d861a4b0a11fb85f24ead84a
digitalWestie <giannichan@gmail.com>
2015-03-24 18:05:54 +0000

    changes error message for link

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,4 @@
 class Link < ActiveRecord::Base
   belongs_to :assessment_answer
-  validates_format_of :link, :with => URI::regexp(%w(http https))
+  validates_format_of :link, :with => URI::regexp(%w(http https)), message: "url is an invalid format"
 end
