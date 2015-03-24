eb61518646dc3e33528fac1c32d7e48cdeced889
digitalWestie <giannichan@gmail.com>
2015-03-24 15:39:33 +0000

    updates controller actions to match route params

--- app/controllers/organisation_admins_controller.rb
+++ app/controllers/organisation_admins_controller.rb
@@ -1,12 +1,13 @@
 class OrganisationAdminsController < ApplicationController
 
   def new_contact
-    @organisation = Organisation.find(params[:id])
+    @organisation = Organisation.find(params[:organisation_id])
     @email = params[:email]
   end
 
   def contact
-    @organisation = Organisation.find(params[:id])    
+    @organisation = Organisation.find(params[:organisation_id])
+    render text: "A message was sent to the admin of #{@organisation.title}"
   end
 
 end
