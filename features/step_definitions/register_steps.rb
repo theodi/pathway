67503555278e5437798ec8ade132c3db9b434679
digitalWestie <giannichan@gmail.com>
2015-03-24 12:56:58 +0000

    adds new steps for taken org test

--- features/step_definitions/register_steps.rb
+++ features/step_definitions/register_steps.rb
@@ -2,3 +2,9 @@ When(/^I fill in Associated organisation with "([^"]*)"$/) do |value|
   hidden_field = find :xpath, "//input[@id='user_associated_organisation']"
   hidden_field.set value
 end
+
+Given(/^a user is already associated with "(.*?)"$/) do |org|
+  user = FactoryGirl.build(:user)
+  user.associated_organisation = org
+  user.save
+end
