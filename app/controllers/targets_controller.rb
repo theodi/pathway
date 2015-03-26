b71cdf522d7b26414521e87479371e1a9bceab44
digitalWestie <giannichan@gmail.com>
2015-03-26 16:55:32 +0000

    adds form to view and appropriate controler response

--- app/controllers/targets_controller.rb
+++ app/controllers/targets_controller.rb
@@ -4,24 +4,19 @@ class TargetsController < ApplicationController
     @assessment = Assessment.find(params[:assessment_id])
     authorize! :update, @assessment
     @dimensions = Questionnaire.current.dimensions
+    @scores = {}
+    @assessment.scores.each { |s| @scores.merge!(s.activity_id => s) }
   end
 
   def update
     @assessment = Assessment.find(params[:assessment_id])
     authorize! :update, @assessment
     @dimensions = Questionnaire.current.dimensions
-
-    if @score.update_attributes(params[:score])
+    if @assessment.update_targets(params[:targets])
       redirect_to assessments_path, notice: "Successfully saved goals"
     else
       render 'edit'
     end
-  end
-
-  private
-
-  def target_params
-    params.require(:targets).permit(:activity_id, :value)
   end
 
 end
