2424b5a9de7bd9bac60ee2cfcfd0c573ba5b8573
digitalWestie <giannichan@gmail.com>
2015-03-26 16:08:37 +0000

    adds a method to update multiple targets

--- app/controllers/targets_controller.rb
+++ app/controllers/targets_controller.rb
@@ -1,18 +1,27 @@
 class TargetsController < ApplicationController
 
   def edit
-    @score = Score.find(params[:score_id])
-    @assessment = @score.assessment
+    @assessment = Assessment.find(params[:assessment_id])
     authorize! :update, @assessment
     @dimensions = Questionnaire.current.dimensions
   end
 
-
   def update
-    @score = Score.find(params[:score_id])
-    @assessment = @score.assessment
+    @assessment = Assessment.find(params[:assessment_id])
     authorize! :update, @assessment
     @dimensions = Questionnaire.current.dimensions
+
+    if @score.update_attributes(params[:score])
+      redirect_to assessments_path, notice: "Successfully saved goals"
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def target_params
+    params.require(:targets).permit(:activity_id, :value)
   end
 
 end
--- spec/models/assessment_spec.rb
+++ spec/models/assessment_spec.rb
@@ -39,6 +39,19 @@ describe Assessment do
       expect( Score.first.score ).to eql(1)
     end
 
+    describe "#update_targets" do 
+      it "should change all the scores for an assessment" do
+        score_targets = {}
+        assessment.complete
+        Rails.logger.info("\n #{assessment.scores.count} scores to change \n")
+        assessment.scores.each { |s| score_targets.merge!(s.id.to_s => rand(1..5)) }
+        assessment.update_targets(score_targets)
+        scores_changed = true
+        score_targets.each {|score_id, target| scores_changed = false if Score.find(score_id).target != target }
+        expect(scores_changed).to eq(true)
+      end
+    end
+
     describe "#next_activity" do
       it "should return the next unstarted activity" do
         activity = Activity.first
