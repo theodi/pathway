36d51423ff4d571b1286242f7e68152970876c76
Leigh Dodds <leigh@ldodds.com>
2015-03-28 11:24:41 +0000

    Reorder list of completed assessments

--- app/controllers/assessments_controller.rb
+++ app/controllers/assessments_controller.rb
@@ -1,7 +1,7 @@
 class AssessmentsController < ApplicationController
   def index
     @current_assessment = current_user.current_assessment
-    @assessments = current_user.assessments.completed.order(:completion_date)
+    @assessments = current_user.assessments.completed.order(completion_date: :desc)
     @last_assessment = @assessments.first
   end
 
