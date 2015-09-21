ad1c24994ea157c01d32e6c55ba60decc69f5015
pezholio <pezholio@gmail.com>
2015-09-21 16:54:08 +0100

    Add test for standard organisation search

--- spec/controllers/organisations_controller_spec.rb
+++ spec/controllers/organisations_controller_spec.rb
@@ -2,4 +2,15 @@ require 'spec_helper'
 
 describe OrganisationsController do
 
+  it "should query an organisation by title" do
+    FactoryGirl.create(:organisation, name: "my-terrrible-organisation", title: "My Terrible Organisation")
+    FactoryGirl.create(:organisation, name: "my-fantastic-organisation", title: "My Fantastic Organisation")
+
+    get "index", q: "fantastic", format: "json"
+
+    results = JSON.parse(response.body)
+
+    expect(results.count).to eq(1)
+    expect(results.first["text"]).to eq("My Fantastic Organisation")
+  end
 end
