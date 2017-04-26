07451f87234d3f80cb736983480aeec7bb75c25f
James Smith <james@floppy.org.uk>
2017-04-26 15:41:42 +0100

    ignore hidden elements again
    it’s a bad smell

--- features/support/env.rb
+++ features/support/env.rb
@@ -14,7 +14,7 @@ Capybara.javascript_driver = :poltergeist
 # selectors in your step definitions to use the XPath syntax.
 # Capybara.default_selector = :xpath
 
-Capybara.ignore_hidden_elements = false
+#Capybara.ignore_hidden_elements = false
 
 # By default, any exception happening in your Rails application will bubble up
 # to Cucumber so that your scenario will fail. This is a different from how
